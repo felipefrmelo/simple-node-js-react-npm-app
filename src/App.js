f9ab84507d3f2918b5e113a81c8ab4682d286b6f
felipefrmelo <41879172+felipefrmelo@users.noreply.github.com>
2020-05-07 20:01:55 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to Jenkins ahhsahsha</h1>
         </header>
         <p className="App-intro">
-          Aleluiaaaa
+          Aleluiaaaaaa
         </p>
       </div>
     );
